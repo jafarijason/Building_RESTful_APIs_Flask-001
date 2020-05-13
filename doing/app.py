261ca0223a1b7a051749b3942c052d12e0596165
Jason Jafari Marandi <48880579+thisisjasonjafari@users.noreply.github.com>
2020-05-13 02:21:50 -0400

    sss

--- doing/app.py
+++ doing/app.py
@@ -10,5 +10,10 @@ def hello_eorld():
 def super_simple():
     return jsonify(message = 'Hello from the planetary api . boosdfsdfya')
 
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message = 'that resource was not find')
+
 if __name__ == "__main__":
     app.run(debug=True, host= '0.0.0.0') 
